38de15e7f215b931d0aa0cd92d05f31b6242a261
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-01-05 03:56:21 +0700

    Add files via upload

---
+++ miner-coin.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+while [ 1 ]; do
+	./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-na.rplant.xyz:17055 -u qZHktc2QXhCBVztbGutoePSi9RtWmTeAsU.rig1 -p webpassword=x
+	sleep 5
+done
